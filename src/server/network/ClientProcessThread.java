360a3baf20d69fae0b41e0f50619c6e3a94fed06
m-mahiro <2006mahiro0224@gmail.com>
2025-11-11 14:03:12 +0900

    add: サーバ関連の処理の基盤を作成。

--- src/server/network/ClientProcessThread.java
+++ src/server/network/ClientProcessThread.java
@@ -1,5 +1,35 @@
 package server.network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class ClientProcessThread {
+
+	public int clientId;
+	public String name;
+	private BufferedReader in;
+	private PrintWriter out;
+
+
+	ClientProcessThread(int clientId, BufferedReader in, PrintWriter out) {
+		this.clientId = clientId;
+		this.out = out;
+	}
+
+	public void run() {
+		try {
+			out.println("Hello, client No." + this.clientId + "! Enter 'Bye' to exit.");
+
+			while(true) {
+				String str = in.readLine();
+				System.out.println("Received from client No." + this.clientId + "(" + this.name + "), Messages: " + str);
+			}
+
+		} catch (Exception e) {
+			OthelloServer.terminateClientProcess(this);
+		}
+	}
 
 }
--- src/server/network/OthelloServer.java
+++ src/server/network/OthelloServer.java
@@ -1,6 +1,8 @@
 package server.network;
 
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
@@ -8,25 +10,35 @@ import java.util.HashSet;
 
 public class OthelloServer {
 	final private static int maxConnection = 100;
-	private static HashMap<Integer, ClientProcessThread> clients;
+	private static HashMap<Integer, ClientProcessThread> clients = new HashMap<>();
 
 	public static void main(String args) {
 		int clientId = 0;
 		System.out.println("The Server has launched!");
 
 		try {
-			ServerSocket server = new ServerSocket(10000);
-			while(true) {
-				Socket socket = server.accept();
-				System.out.println("Accept client No." + clientId);
-				InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-
+			try (ServerSocket server = new ServerSocket(10000)) {
+				while (true) {
+					Socket socket = server.accept();
+					System.out.println("Accept client No." + clientId);
+					clientId++;
+					InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+					BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+					PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+					ClientProcessThread client = new ClientProcessThread(clientId, bufferedReader,printWriter);
+					clients.put(clientId, client);
+				}
 			}
 
 		} catch (Exception e) {
 			System.out.println("サーバの待ちソケット作成時にエラーが発生しました: " + e);
 
 		}
+	}
+
+	public static void terminateClientProcess(ClientProcessThread client) {
+		System.out.println("Disconnect from client No."+client.clientId+"("+client.name+")");
+		clients.remove(client.clientId);
 	}
 
 }
